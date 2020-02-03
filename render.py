2bf8db8b6d0da1de8b81f2ff8170e589a1296985
Alex Reinking <alex_reinking@berkeley.edu>
2020-02-03 13:52:55 -0800

    Fuse everything together to run faster.

--- render.py
+++ render.py
@@ -19,45 +19,34 @@ def circle_as_4_tuple(center, size):
     return (center[0] - size, center[1] - size, center[0] + size, center[1] + size)
 
 
-lines = None
-with open(input_file, 'r') as f:
-    lines = f.readlines()
-
-num_parts = int(lines[0].split()[0])
-boxsize = float(lines[0].split()[1])
-
-data = [[]]
-# Data is a list of lists
-# Outer list represents time steps
-# Inner list represents all point (x,y)'s in that time step
-# There is probably a better way to do this
-
-# Parse input file
-for line in lines[1:]:
-
-    if line.isspace():
-        data.append([])
-        continue
-
-    line_split = line.split()
-    center_x = int(1024 * (float(line_split[0]) / boxsize))
-    center_y = int(1024 * (float(line_split[1]) / boxsize))
-    data[-1].append((center_x, center_y))
-
-# Remove trailing spaces
-while len(data[-1]) == 0:
-    data = data[:-1]
-
-# Render GIF
 frames = []
-for data_frame in data:
-    img = Image.new('L', (1024, 1024), 'white')
-    drawer = ImageDraw.Draw(img)
-    for data_point in data_frame:
-        if cutoff:
-            drawer.ellipse(circle_as_4_tuple(data_point, int(1024 * (cutoff / boxsize))), 'yellow')
-        drawer.ellipse(circle_as_4_tuple(data_point, 1), 'black')
-    frames.append(img)
 
-# Save output
-frames[0].save(output_file, format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
+
+def new_frame():
+    img = Image.new('L', (1024, 1024), 'white')
+    frames.append(img)
+    return ImageDraw.Draw(img)
+
+
+with open(input_file, 'r') as f:
+    num_parts, box_size = next(f).split()
+    num_parts, box_size = int(num_parts), float(box_size)
+
+    # Parse input file
+    drawer = new_frame()
+    for line in f:
+        if line.isspace():
+            drawer = new_frame()
+            continue
+
+        line_split = line.split()
+        center_x = int(1024 * (float(line_split[0]) / box_size))
+        center_y = int(1024 * (float(line_split[1]) / box_size))
+        center = (center_x, center_y)
+
+        if cutoff:
+            cutuff_radius = int(1024 * (cutoff / box_size))
+            drawer.ellipse(circle_as_4_tuple(center, cutuff_radius), 'yellow')
+        drawer.ellipse(circle_as_4_tuple(center, 1), 'black')
+
+    frames[0].save(output_file, format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
