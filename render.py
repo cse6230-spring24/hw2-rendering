9287f909306f007d40ece79ce0364f40411b3db1
Alex Reinking <alex_reinking@berkeley.edu>
2020-02-03 14:29:13 -0800

    Save some lines with itertools

--- render.py
+++ render.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import sys
-from sys import argv
+from itertools import groupby
+from sys import argv, exit
 
 from PIL import Image
 from PIL import ImageDraw
 
 # Parse Command Line Arguments
 if len(argv) < 3:
-    sys.exit("Usage is render.py <input file> <output file> [cutoff]")
+    exit("Usage is render.py <input file> <output file> [cutoff]")
 
 input_file = argv[1]
 output_file = argv[2]
@@ -15,38 +15,33 @@ cutoff = None if len(argv) <= 3 else float(argv[3])
 
 
 # Helper Functions
-def circle_as_4_tuple(center, size):
-    return (center[0] - size, center[1] - size, center[0] + size, center[1] + size)
-
-
-frames = []
-
-
-def new_frame():
-    img = Image.new('L', (1024, 1024), 'white')
-    frames.append(img)
-    return ImageDraw.Draw(img)
+def circle_as_4_tuple(center_x, center_y, size):
+    return (center_x - size, center_y - size,
+            center_x + size, center_y + size)
 
 
 with open(input_file, 'r') as f:
+    # Get first line to find the number of particles and the box size.
     num_parts, box_size = next(f).split()
     num_parts, box_size = int(num_parts), float(box_size)
 
     # Parse input file
-    drawer = new_frame()
-    for line in f:
-        if line.isspace():
-            drawer = new_frame()
-            continue
+    frames = []
+    file_sections = groupby(f, lambda x: x and not x.isspace())
+    frame_sections = (x[1] for x in file_sections if x[0])
+    for frame_section in frame_sections:
+        img = Image.new('L', (1024, 1024), 'white')
+        drawer = ImageDraw.Draw(img)
+        frames.append(img)
 
-        line_split = line.split()
-        center_x = int(1024 * (float(line_split[0]) / box_size))
-        center_y = int(1024 * (float(line_split[1]) / box_size))
-        center = (center_x, center_y)
+        for line in frame_section:
+            center_x, center_y = line.split()
+            center_x = int(1024 * (float(center_x) / box_size))
+            center_y = int(1024 * (float(center_y) / box_size))
 
-        if cutoff:
-            cutuff_radius = int(1024 * (cutoff / box_size))
-            drawer.ellipse(circle_as_4_tuple(center, cutuff_radius), 'yellow')
-        drawer.ellipse(circle_as_4_tuple(center, 1), 'black')
+            if cutoff:
+                cutuff_radius = int(1024 * (cutoff / box_size))
+                drawer.ellipse(circle_as_4_tuple(center_x, center_y, cutuff_radius), 'yellow')
+            drawer.ellipse(circle_as_4_tuple(center_x, center_y, 1), 'black')
 
     frames[0].save(output_file, format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
